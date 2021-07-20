d2f4649a83f0a11dab5da158feec8c0830afa923
Vadim Brikulya <im.master99@gmail.com>
2021-07-20 16:20:48 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from "./logo.svg";
-import "./App.css";
+import s from "./App.module.css";
+import React, { Component } from "react";
+import Statistics from "./components/Statistics/Statistics";
+import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
+import Section from "./components/Section/Section";
+// import Notification from './components/Notification/Notification'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  static defaultProps = {};
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  countTotalFeedback = (e) => {
+    return e;
+  };
+
+  countPositiveFeedbackPercentage = (e) => {
+    return Math.round((this.state.good * 100) / e);
+  };
+
+  leaveFeedback = (e) => {
+    this.setState((prevState) => {
+      return { [e]: prevState[e] + 1 };
+    });
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div className={s.App}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.leaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback}
+            positivePercentage={this.countPositiveFeedbackPercentage}
+          />
+        </Section>
+      </div>
+    );
+  }
 }
-
-export default App;
